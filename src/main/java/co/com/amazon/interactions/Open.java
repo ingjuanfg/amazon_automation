b535f5fd077e2c32d98a57532485a26acad650d7
ingjuanfg <juan.isfg@gmail.com>
2020-03-04 00:21:01 -0500

    Se crea clase para driver web

--- src/main/java/co/com/amazon/interactions/Open.java
+++ src/main/java/co/com/amazon/interactions/Open.java
@@ -5,6 +5,7 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.serenitybdd.screenplay.Performable;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -16,18 +17,11 @@ import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class Open{
 
-        public static WebDriver app() {
-            WebDriver driver = null;
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("driver", "chrome");
-            capabilities.setCapability("chrome.driver", "C:/chromedriver.exe");
-            capabilities.setCapability("wait.for.timeout ", "16000");
-            capabilities.setCapability("timeouts.implicitlywait", "1600");
-            try {
-                driver = new RemoteWebDriver(new URL("http://www.google.com"), capabilities);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            return driver;
-        }
+    public static WebDriver getDriver(){
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com/-/es/");
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
--- src/test/java/co/com/amazon/stepdefinitions/Hooks.java
+++ src/test/java/co/com/amazon/stepdefinitions/Hooks.java
@@ -1,23 +1,19 @@
 package co.com.amazon.stepdefinitions;
 
 import co.com.amazon.interactions.Open;
-import co.com.amazon.interactions.OpenThe;
 import cucumber.api.java.Before;
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.thucydides.core.annotations.Managed;
 import org.openqa.selenium.WebDriver;
 
-import static co.com.amazon.interactions.Open.app;
 import static co.com.amazon.utils.DriverManager.amazonCapabilities;
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getProxiedDriver;
 
 public class Hooks {
- //   @Managed(driver = "chrome")
-  //  private WebDriver theBrowser;
+   @Managed(driver = "chrome")
+    private WebDriver theBrowser;
     public static final Actor Daniel = Actor.named("Daniel");
 
     @Before
@@ -27,9 +23,8 @@ public class Hooks {
 
     @Before("@Web")
     public void webInitialization() {
-        Daniel.attemptsTo((Performable) app());
-        Daniel.can(BrowseTheWeb.with(app()));
-        Daniel.wasAbleTo(OpenThe.browser());
+        Daniel.can(BrowseTheWeb.with(Open.getDriver()));
+        //Daniel.attemptsTo(Open.app());
     }
 
     @Before("@Desktop")
